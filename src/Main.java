061b9c0eedb5c183e5e981a523c316e602a77f9d
chris.d.bettis@gmail.com <chris.d.bettis@gmail.com>
2018-03-09 11:27:48 -0800

    added .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        if(Util.millerRabin(2,2047))
+        if(Util.millerRabin(1001,2))
             System.out.println("2047 is a prime");
     }
 }
